935241626e8ce0cc9ce94ad307d2baf3f4567df7
webmaster442 <ruzsinszki.gabor@gmail.com>
2014-10-01 13:07:43 +0200

    examples for FastDigital and PortLib

---
+++ libraries/FastDigital/examples/FastDigital/FastDigital.ino
@@ -0,0 +1,22 @@
+//include FastDigital lib
+#include <FastDigital.h>
+
+//Setup pins
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  //pin 13 ON
+  WriteD13(HIGH);
+  //pin 13 OFF
+  WriteD13(LOW);
+  
+  //read pin 11 and send it's value trouh serial
+  int value = ReadD11();
+  Serial.println(value);
+}
+
---
+++ libraries/PortLib/examples/PortLibDemo/PortLibDemo.ino
@@ -0,0 +1,18 @@
+//Include portlib
+#include <PortLib.h>
+
+//creates a port using pins: 3, 4, 5, 6. PIN3 is LSB. Direction is output
+Port4 port(3, 4, 5, 6, OUTPUT);
+
+void setup()
+{
+}
+
+void loop()
+{
+  //Binary counter
+  for (int i=0; i<16; i++)
+  {
+    port.Write(i);
+  }
+}
